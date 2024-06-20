dd5f7bd1b8be42a2993ae60d04f86738dc587ffb
Aaron Pumm <pummaaron@gmail.com>
2024-06-20 16:47:48 +0200

    modified:   test.py

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 import numpy as np
 
+
 def visualize_dict(dictionary):
-    # Extract coordinates and values
-    coords = list(dictionary.keys())
+    # Extract cell_numinates and values
+    cell_nums = list(dictionary.keys())
     values = list(dictionary.values())
 
-    if len(coords) == 0:
+    if len(cell_nums) == 0:
         return
 
     # Determine the size of the matrix
-    max_x = max(coords, key=lambda x: x[0])[0]
-    max_y = max(coords, key=lambda x: x[1])[1]
-    min_x = min(coords, key=lambda x: x[0])[0]
-    min_y = min(coords, key=lambda x: x[1])[1]
+    max_x = max(cell_nums, key=lambda x: x[0])[0]
+    max_y = max(cell_nums, key=lambda x: x[1])[1]
+    min_x = min(cell_nums, key=lambda x: x[0])[0]
+    min_y = min(cell_nums, key=lambda x: x[1])[1]
 
     # Create a matrix filled with spaces
     matrix = np.full((max_y - min_y + 1, max_x - min_x + 1), ' ')
@@ -42,18 +43,31 @@ def visualize_dict(dictionary):
 
     print()
 
-def draw_board(coordinates):
+
+def draw_board(cell_numinates):
     # Clear the screen
     print("\033[H\033[J", end="")
 
     # Draw the board
     for y in range(10):
         for x in range(10):
-            if (x, y) in coordinates:
+            if (x, y) in cell_numinates:
                 print("X", end="")
             else:
                 print(".", end="")
         print()
+
+
+def coord_to_num(coord: tuple[int, int], board_size: tuple[int, int]) -> int:
+    x, y = coord
+    a, b = board_size
+    return x+y*a
+
+
+def num_to_coord(num: int, board_size: tuple[int, int]) -> tuple[int, int]:
+    a, b = board_size
+    return (num % a, num//b)
+
 
 def within_board(ship, board_size):
     for x, y in ship:
@@ -69,6 +83,7 @@ def within_board(ship, board_size):
 
     return True
 
+
 def pad_ship(ship, board_size):
     padded_ship = set([])
     for (x, y) in ship:
@@ -78,30 +93,80 @@ def pad_ship(ship, board_size):
                     padded_ship.add((x+a, y+b))
     return padded_ship
 
-def generate_ship_positions(ships, board_size):
-    ship_positions = []
-    padded_ships_positions = []
-    for ship in ships:
-        ship_name, ship_len, ship_count = ship
-        for i in range(ship_count):
-            ship_positions.append([])
-            padded_ships_positions.append([])
-            for x in range(board_size[0]):
-                for y in range(board_size[1]):
-                    ship = [(x+k, y) for k in range(ship_len)]
-                    if within_board(ship, board_size):
-                        ship = set(ship)
-                        padded_ship = pad_ship(ship, board_size)
-                        ship_positions[-1].append(ship)
-                        padded_ships_positions[-1].append(padded_ship)
-                    ship = [(x, y+k) for k in range(ship_len)]
-                    if within_board(ship, board_size):
-                        ship = set(ship)
-                        padded_ship = pad_ship(ship, board_size)
-                        ship_positions[-1].append(ship)
-                        padded_ships_positions[-1].append(padded_ship)
 
-    return ship_positions, padded_ships_positions
+def generate_ship_positions(ships_details, board_size) -> list[dict[int, list[tuple[set[int], set[int], int]]]]:
+    ship_positions = []
+    for ship_details in ships_details:
+        ship_name, ship_len, ship_count = ship_details
+        for i in range(ship_count):
+            ship_positions.append({})
+            pos_id = 0
+            for y in range(board_size[0]):
+                for x in range(board_size[1]):
+
+                    cell_num = coord_to_num((x, y), board_size)
+                    ship_positions[-1][cell_num] = []
+
+                    ship_coords = [(x+k, y) for k in range(ship_len)]
+                    if within_board(ship_coords, board_size):
+
+                        ship_nums = set([coord_to_num(coord, board_size)
+                                        for coord in ship_coords])
+                        padded_ship_coords = pad_ship(ship_coords, board_size)
+                        padded_ship_nums = set(
+                            [coord_to_num(coord, board_size) for coord in padded_ship_coords])
+
+                        ship_positions[-1][cell_num].append(
+                            (ship_nums, padded_ship_nums, pos_id))
+                        pos_id += 1
+
+                    ship_coords = [(x, y+k) for k in range(ship_len)]
+                    if within_board(ship_coords, board_size):
+
+                        ship_nums = set([coord_to_num(coord, board_size)
+                                        for coord in ship_coords])
+                        padded_ship_coords = pad_ship(ship_coords, board_size)
+                        padded_ship_nums = set(
+                            [coord_to_num(coord, board_size) for coord in padded_ship_coords])
+
+                        ship_positions[-1][cell_num].append(
+                            (ship_nums, padded_ship_nums, pos_id))
+                        pos_id += 1
+
+    return ship_positions
+
+
+def recursion(ships_positions: list[dict[int, list[tuple[set[int], set[int], int]]]], cell_ranges: list[list[int]], redundancy_indices:list[int], pos_ids: list[int] = [], i: int = 0, board: set[int] = set([])) -> None:
+
+    
+    search_space = [cell_num for cell_num in cell_ranges[i] if cell_num not in board]
+    # ships starting from cell with cell num
+    for cell_num in search_space:
+        # both x-axis and y-axis versions
+        for ship, padded_ship, pos_id in ships_positions[i][cell_num]:
+            
+            if i in redundancy_indices and pos_ids[-1] >= pos_id:
+                continue
+                
+            if padded_ship.isdisjoint(board):
+
+                if i == len(ships_positions)-1:  # leaf condition
+
+                    global densities
+                    global k
+
+                    for cell_num in board.union(ship):
+                        densities[cell_num] += 1
+
+                    if k % 10_000 == 0:
+                        print("\033[H\033[J", end="")
+                        print(pos_ids, end="\r")
+                    k += 1
+                    return
+
+                else:
+
+                    recursion(ships_positions, cell_ranges, redundancy_indices, pos_ids + [pos_id], i+1, board.union(ship))
 
 
 ships = [("Schlachtschiff", 6, 1), ("Kreuzer", 4, 2),
@@ -109,39 +174,20 @@ ships = [("Schlachtschiff", 6, 1), ("Kreuzer", 4, 2),
 
 board_size = (10, 10)
 
-ships_positions, padded_ships_positions = generate_ship_positions(
-    ships, board_size)
+ships_positions = generate_ship_positions(ships, board_size)
+
+cell_ranges = [range(board_size[0]*board_size[1])]*len(ships_positions)
 
 densities = {}
+for i in range(board_size[0]*board_size[1]):
+    densities[i] = 0
+
 k = 0
 
-def recursion(ships_positions, padded_ships_positions, densities, redundancy_indices, ship_indices=[], i=0, board=set([])):
+# for ship_positions in ships_positions:
+#     print()
+#     for cell_num in ship_positions:
+#         print(cell_num, ship_positions[cell_num])
 
-    if i == len(ships_positions):
 
-        for coord in board:
-            densities[coord] = densities.setdefault(coord, 0) + 1
-        global k
-        if k % 1_000_000 == 0:
-            draw_board(board)
-            print(ship_indices)
-            visualize_dict(densities)
-            # Move the cursor back to the top left
-            print("\033[H", end="")
-        k += 1
-        return
-
-    if i in redundancy_indices:
-        redundancy_start = ship_indices[-1] + 1
-    else:
-        redundancy_start = 0
-
-    for j in range(redundancy_start, len(ships_positions[i])):
-        ship = ships_positions[i][j]
-        padded_ship = padded_ships_positions[i][j]
-        if padded_ship.isdisjoint(board):
-            recursion(ships_positions, padded_ships_positions, densities,
-                      redundancy_indices, ship_indices + [j], i+1, board.union(ship))
-
-recursion(ships_positions, padded_ships_positions,
-          densities, [2, 4, 5, 7, 8, 9])
+recursion(ships_positions, cell_ranges, [2,4,5,7,8,9])
